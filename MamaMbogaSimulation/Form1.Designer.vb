b3d14227bfa6fa7ab1d10eb99bc0a08305c78b6e
danny <danny@MASTERMIND>
2026-02-02 12:32:24 +0300

    Add project files.

---
+++ MamaMbogaSimulation/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnArrive = New System.Windows.Forms.Button()
+        Me.btnAskPrice = New System.Windows.Forms.Button()
+        Me.btnBargain = New System.Windows.Forms.Button()
+        Me.btnBuy = New System.Windows.Forms.Button()
+        Me.btnStock = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lstLog = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnArrive
+        '
+        Me.btnArrive.Location = New System.Drawing.Point(360, 12)
+        Me.btnArrive.Name = "btnArrive"
+        Me.btnArrive.Size = New System.Drawing.Size(114, 39)
+        Me.btnArrive.TabIndex = 0
+        Me.btnArrive.Text = "Customer Arrives"
+        Me.btnArrive.UseVisualStyleBackColor = True
+        '
+        'btnAskPrice
+        '
+        Me.btnAskPrice.Location = New System.Drawing.Point(360, 48)
+        Me.btnAskPrice.Name = "btnAskPrice"
+        Me.btnAskPrice.Size = New System.Drawing.Size(114, 38)
+        Me.btnAskPrice.TabIndex = 1
+        Me.btnAskPrice.Text = "Ask Price"
+        Me.btnAskPrice.UseVisualStyleBackColor = True
+        '
+        'btnBargain
+        '
+        Me.btnBargain.Location = New System.Drawing.Point(360, 85)
+        Me.btnBargain.Name = "btnBargain"
+        Me.btnBargain.Size = New System.Drawing.Size(114, 27)
+        Me.btnBargain.TabIndex = 2
+        Me.btnBargain.Text = "Bargain"
+        Me.btnBargain.UseVisualStyleBackColor = True
+        '
+        'btnBuy
+        '
+        Me.btnBuy.Location = New System.Drawing.Point(360, 109)
+        Me.btnBuy.Name = "btnBuy"
+        Me.btnBuy.Size = New System.Drawing.Size(114, 23)
+        Me.btnBuy.TabIndex = 3
+        Me.btnBuy.Text = "Pay"
+        Me.btnBuy.UseVisualStyleBackColor = True
+        '
+        'btnStock
+        '
+        Me.btnStock.Location = New System.Drawing.Point(360, 129)
+        Me.btnStock.Name = "btnStock"
+        Me.btnStock.Size = New System.Drawing.Size(114, 43)
+        Me.btnStock.TabIndex = 4
+        Me.btnStock.Text = "Add Stock"
+        Me.btnStock.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(333, 175)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(107, 13)
+        Me.lblStatus.TabIndex = 5
+        Me.lblStatus.Text = "Current system status"
+        '
+        'lstLog
+        '
+        Me.lstLog.FormattingEnabled = True
+        Me.lstLog.Location = New System.Drawing.Point(-1, 195)
+        Me.lstLog.Name = "lstLog"
+        Me.lstLog.Size = New System.Drawing.Size(789, 238)
+        Me.lstLog.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstLog)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnStock)
+        Me.Controls.Add(Me.btnBuy)
+        Me.Controls.Add(Me.btnBargain)
+        Me.Controls.Add(Me.btnAskPrice)
+        Me.Controls.Add(Me.btnArrive)
+        Me.Name = "Form1"
+        Me.Text = " mamabongasimulation"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnArrive As Button
+    Friend WithEvents btnAskPrice As Button
+    Friend WithEvents btnBargain As Button
+    Friend WithEvents btnBuy As Button
+    Friend WithEvents btnStock As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lstLog As ListBox
+End Class
---
+++ MamaMbogaSimulation/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Public Event CustomerArrived(trigger As String)
+    Public Event PriceAsked(trigger As String)
+    Public Event PriceNegotiated(trigger As String)
+    Public Event PaymentMade(trigger As String)
+    Public Event StockUpdated(trigger As String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler CustomerArrived, AddressOf HandleCustomerArrived
+        AddHandler PriceAsked, AddressOf HandlePriceAsked
+        AddHandler PriceNegotiated, AddressOf HandlePriceNegotiated
+        AddHandler PaymentMade, AddressOf HandlePaymentMade
+        AddHandler StockUpdated, AddressOf HandleStockUpdated
+        lstLog.Items.Add("TIME     | EVENT NAME         | TRIGGER")
+        lstLog.Items.Add("-------------------------------------------")
+
+        btnAskPrice.Enabled = False
+        btnBargain.Enabled = False
+        btnBuy.Enabled = False
+        btnStock.Enabled = True
+    End Sub
+
+    Private Sub HandleCustomerArrived(trigger As String)
+        lblStatus.Text = "Customer has arrived"
+        LogEvent("CustomerArrived", trigger)
+
+        btnAskPrice.Enabled = True
+    End Sub
+
+    Private Sub HandlePriceAsked(trigger As String)
+        lblStatus.Text = "Customer asked for price"
+        LogEvent("PriceAsked", trigger)
+
+        btnBargain.Enabled = True
+    End Sub
+
+    Private Sub HandlePriceNegotiated(trigger As String)
+        lblStatus.Text = "Price negotiated"
+        LogEvent("PriceNegotiated", trigger)
+
+        btnBuy.Enabled = True
+    End Sub
+
+    Private Sub HandlePaymentMade(trigger As String)
+        lblStatus.Text = "Payment made"
+        LogEvent("PaymentMade", trigger)
+
+        btnAskPrice.Enabled = False
+        btnBargain.Enabled = False
+        btnBuy.Enabled = False
+    End Sub
+
+    Private Sub HandleStockUpdated(trigger As String)
+        lblStatus.Text = "Stock updated"
+        LogEvent("StockUpdated", trigger)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnBargain.Click
+        RaiseEvent PriceNegotiated("Button Click")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnBuy.Click
+        RaiseEvent PaymentMade("Button Click")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnStock.Click
+        RaiseEvent StockUpdated("Button Click")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnAskPrice.Click
+        RaiseEvent PriceAsked("Button Click")
+    End Sub
+
+    Private Sub btnArrive_Click(sender As Object, e As EventArgs) Handles btnArrive.Click
+        RaiseEvent CustomerArrived("Button Click")
+    End Sub
+
+
+    Private Sub LogEvent(eventName As String, trigger As String)
+        lstLog.Items.Add($"{DateTime.Now:HH:mm} | {eventName,-18} | {trigger}")
+    End Sub
+
+
+    Private Sub lstLog_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstLog.SelectedIndexChanged
+        If lstLog.SelectedItem IsNot Nothing Then
+            lblStatus.Text = "Selected Log: " & lstLog.SelectedItem.ToString()
+        End If
+    End Sub
+
+End Class
